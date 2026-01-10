c5d64c266ab233305bc8d8f19626954a06a7bb8d
Kiryl Zarouski <zarouski@MacBook-Pro.local>
2026-01-11 01:36:02 +0300

    fix proto

--- db/influencedb/influencedb.sql
+++ db/influencedb/influencedb.sql
@@ -27,9 +27,19 @@ CREATE TYPE influence.simulation_state AS ENUM (
     'Failed'
 );
 
+CREATE TABLE influence.users_simulations(
+    uid BIGINT NOT NULL REFERENCES influence.users(id),
+    sid UUID NOT NULL REFERENCES influence.simulations(id),
+    number INT NOT NULL
+)
+
+CREATE INDEX users_simulations_uid_index ON influence.users_simulations (uid);
+CREATE INDEX users_simulations_sid_index ON influence.users_simulations (sid);
+
 CREATE TABLE influence.simulations(
     id UUID UNIQUE DEFAULT gen_random_uuid(),
-    data JSONB
+    map_id UUID NOT NULL REFERENCES influence.maps(id),
+    data JSONB,
     queued_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
     started_at TIMESTAMP WITH TIME ZONE,
     finished_at TIMESTAMP WITH TIME ZONE,
@@ -42,6 +52,7 @@ CREATE INDEX simulations_created_at_index ON influence.replays (created_at);
 CREATE TABLE influence.maps(
     id UUID UNIQUE DEFAULT gen_random_uuid(),
     data JSONB,
+    name TEXT NOT NULL,
     meta TEXT
 )
 
